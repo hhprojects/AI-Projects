62b4fd2a72ae72593a486c3e8749e8809cfad900
Han Hua <lhanhua01@gmail.com>
2025-03-09 12:53:25 +0800

    Completed 3 state MDP problem

--- 3_state.py
+++ 3_state.py
@@ -1,14 +1,13 @@
-import numpy as np
 import pandas as pd
 
-#Define MDP Parameters
+# Define MDP Parameters
 states = ["s0", "s1", "s2"]
 actions = ["a0", "a1"]
 gamma = 0.9 #Discount Factor
-theta = 1e-4 #Convergence Threshold
+epsilon = 1e-3 #Convergence Threshold
 
-#Transition probabilities and reward (s', P, R) for (s, a) -> s'
-transition_rewards = {
+# Transition probabilities and reward (s', P, R) for (s, a) -> s'
+transition_model = {
     "s0": {
         "a0": [("s0", 0.5, 0), ("s2", 0.5, 0)],
         "a1": [("s2", 1, 0)]
@@ -23,6 +22,51 @@ transition_rewards = {
     }
 }
 
-#Initialize value function
+# Initialize value function
 V = {s: 0 for s in states}
 
+count = 0
+# Perform value iteration
+while True:
+    delta = 0
+    v = V.copy()
+    
+    print(f"""
+          iter  {count}  |   V(s0) = {round(v[states[0]],2)}   V(s1) = {round(v[states[1]],2)}   V(s0) = {round(v[states[2]],2)}
+          """)
+    
+    for s in states:
+        v[s] = max(sum(p * (r + gamma * V[s_next]) for s_next, p, r in transition_model[s][a]) for a in actions) #Bellman Optimality Equation
+        delta = max(delta, abs(v[s] - V[s]))
+    
+    V = v # Update value function with new values
+    
+    # Check for convergence
+    if delta < epsilon:
+        break
+    
+    count += 1
+    
+# Extract optimal policy
+policy = {}
+for s in states:
+    best_action = None
+    best_value = -float("inf")
+
+    for a in actions:
+        value = sum(p * (r + gamma * V[s_next]) for s_next, p, r in transition_model[s][a])
+        if value > best_value:
+            best_value = value
+            best_action = a
+
+    policy[s] = best_action
+    
+    
+# DataFrame to display the Value Function and Optimal Policy
+df_combined = pd.DataFrame({
+    "State": list(V.keys()),
+    "Value": list(V.values()),
+    "Optimal Action": [policy[s] for s in V.keys()]
+})
+
+print(df_combined)
